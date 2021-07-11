f16448cd4d1cdf30ead992d790941edf46645603
KazeYan <77669862+aaawilly136@users.noreply.github.com>
2021-07-11 17:07:37 +0800

    新增採集物特效及採集判定 與史帝夫

---
+++ 3D_MINECRAFT/Assets/Script/ObjectCollection.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 採集物件:儲存採集物件資料以及功能
+/// </summary>
+public class ObjectCollection : MonoBehaviour
+{
+    [Header("採集物件資料")]
+    public DataCollection data;
+    [Header("採集物爆炸特效")]
+    public GameObject objExplosion;
+    /// <summary>
+    /// 血量
+    /// 儲存腳本化物件資料提供物件個別使用
+    /// </summary>
+    private float hp;
+    private void Start()
+    {
+        hp = data.hp;
+    }
+    /// <summary>
+    /// 受到攻擊傷害
+    /// </summary>
+    /// <param name="damage"></param>
+    public void Hit(float damage)
+    {
+        hp -= damage;
+        if (hp <= 0) Dead();
+    }
+    /// <summary>
+    /// 採集物件毀損死亡並且掉落採集物的碎片
+    /// </summary>
+    private void Dead()
+    {
+        Destroy(gameObject);
+        Instantiate(data.objDrop, transform.position, Quaternion.Euler(0, 45, 0));
+        Instantiate(objExplosion, transform.position, Quaternion.identity);
+    }
+}
---
+++ 3D_MINECRAFT/Assets/Script/Player.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region 欄位
+    [Header("採集範圍"), Range(0, 30)]
+    public float rangeCollect = 2.5f;
+
+    private Rigidbody rig;
+    private Animator ani;
+    private Transform traCamera;
+
+    #endregion
+    #region 事件
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+        //transform.Find("子物件名稱")透過名稱搜尋子物件
+        ani = transform.Find("男生").GetComponent<Animator>(); 
+        traCamera = GameObject.Find("攝影機").transform;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        //圖示.繪製射線(中心點、射線方向)
+        Gizmos.DrawRay(traCamera.position, traCamera.forward * rangeCollect);
+    }
+    private void Update()
+    {
+        collection();
+    }
+    #endregion
+    #region 方法
+    /// <summary>
+    /// 採集:繪製射線並判定是否攻擊以及採集物件
+    /// </summary>
+    private void collection()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //射線碰撞資訊
+            RaycastHit hit;
+            //物理.射線碰撞(中心點、射線方向、射線碰撞資訊、射線長度、圖層) - out 將資訊儲存在輸入的欄位內
+            if(Physics.Raycast(traCamera.position, traCamera.forward, out hit, rangeCollect, 1 << 8))
+            {
+                //print("射線碰到的物件" + hit.collider.name);
+                //對採集物件造成傷害
+                hit.collider.GetComponent<ObjectCollection>().Hit(1);
+            }
+
+            
+        }
+        
+    }
+    #endregion
+}
